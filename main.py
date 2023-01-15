9980a13ffdd5f9b687132cd223d9a11033e03dc0
Clearsky <ernie60717@gmail.com>
2023-01-15 14:56:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import re
 import time
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+    'lang' : 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.6',
+    'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept_encoding' : 'gzip, deflate',
+    'cache_control' : 'max-age=0'
 }
 
 def folder_name(year,month):
